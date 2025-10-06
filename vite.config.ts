85df57d4a03ff1c0097f5fddae92f26f35fb3dcd
objectfoo <objectfoo@users.noreply.github.com>
2025-10-06 13:35:39 -0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: "index.html",
+				"stack-test-experiment5": "pages/stack-test-exp5.html",
 				"stack-test-experiment4": "pages/stack-test-exp4.html",
 				"stack-test-experiment3": "pages/stack-test-exp3.html",
 				"stack-test-experiment2": "pages/stack-test-exp2.html",
